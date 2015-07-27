f025a9ccb2809948ca1cd505995e99032699564c
josephferrairo <josephferrairo@gmail.com>
2015-07-27 01:07:22 +0000

    added validation for everything finally

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,11 @@
 class Place < ActiveRecord::Base
 
 	belongs_to :user
-	validates :name, :presence => true
+	 validates :name, :presence => true, :length => { :within => 3..100 }
+
+	validates :description, :address, :presence => true
+
+
+
+
 end
