3b3b8e2b48fc75180e2c9276b6f15b16b7218dfe
josephferrairo <josephferrairo@gmail.com>
2015-07-28 23:51:31 +0000

    set up comments db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
 
 	belongs_to :user
+	has_many :comments
 	geocoded_by :address
   	after_validation :geocode
 	 validates :name, :presence => true, :length => { :within => 3..100 }
